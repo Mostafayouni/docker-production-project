ff5f8366c6035b487b78cd5ff5a3a7fcd0f3aa9e
mostafayounis <mostafayounis05@gmail.com>
2026-06-02 13:39:36 +0300

    new start

---
+++ app/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const mongoURL = process.env.MONGO_URL;
+
+mongoose.connect(mongoURL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Production Docker App 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
